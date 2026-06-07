bc54564e57af596895b127a7a71759acf01b6618
kayp2083-glitch <kayp2083@gmail.com>
2026-06-07 04:11:38 -1200

    Change CORS settings to allow all origins

--- main.py
+++ main.py
@@ -425,7 +425,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins     = ["https://sentinelrecover.app", "http://localhost:3000"],
+    allow_origins     = ["*"],
     allow_credentials = True,
     allow_methods     = ["*"],
     allow_headers     = ["*"],
